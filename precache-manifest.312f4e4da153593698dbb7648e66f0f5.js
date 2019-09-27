89c082b6b08deefa644695807d347d9aa1c1a747
ankitpatelgit <ankitpatel118@gmail.com>
2019-09-27 20:53:40 +0530

    Updates

---
+++ precache-manifest.312f4e4da153593698dbb7648e66f0f5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7de5121b132ad5ba784106e62c241d4c",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d28042dc3cc0ec3e2b15",
+    "url": "/monsters-rolodex/static/css/main.6542fc9d.chunk.css"
+  },
+  {
+    "revision": "abc1ce18aadecde23a06",
+    "url": "/monsters-rolodex/static/js/2.e011650a.chunk.js"
+  },
+  {
+    "revision": "d28042dc3cc0ec3e2b15",
+    "url": "/monsters-rolodex/static/js/main.c9d6151e.chunk.js"
+  },
+  {
+    "revision": "0d0f416e7b5fe29edb74",
+    "url": "/monsters-rolodex/static/js/runtime-main.9ce5b26a.js"
+  }
+]);
